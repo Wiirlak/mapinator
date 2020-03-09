fa350b036ba40da3c5a70b174b1b691cb2288596
Luis Valdez <luis.valdez@affluences.com>
2020-03-09 15:48:27 +0100

    first commit

---
+++ src/main/java/com/esgjee/mapinator/MapinatorApplication.java
@@ -0,0 +1,13 @@
+package com.esgjee.mapinator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapinatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapinatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/esgjee/mapinator/MapinatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esgjee.mapinator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapinatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
